5348ceeafd9c68b9590a93ed9a04923e6c254f01
kimbeomjun <gimb52043@gmail.com>
2023-07-03 15:08:04 +0900

    tailwind css 설명

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // tailwind를 사용할 곳
+    // ** : 모든폴더
+    // * : 모든 파일.{확장자명}
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
